ef13854428642a9067fdb433ab0ff8a1e483cc97
Hoang Pham <tienhoang.pham@bc.fi>
2020-10-30 12:43:52 +0200

    Add solution code until task 7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  state = {
+    likes: 0
+  }
+
+  increase = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+
+  decrease = () => {
+    if (this.state.likes == 0) {
+      return;
+    }
+    
+    this.setState({
+      likes: this.state.likes - 1
+    })
+  }
+
+  reset = () => {
+    this.setState({
+      likes: 0
+    })
+  }
+
+  render(){
+    const isEven = this.state.likes % 2 === 0
+    return <div 
+        style={{ backgroundColor: isEven ? 'red' : 'yellow' }}
+        className={'myClass ' + (isEven ? 'even' : 'odd')}
+      >
+
+      Total likes: {this.state.likes}
+      <button id="increase-btn" onClick={this.increase}>Increase</button>
+      <button id="decrease-btn" onClick={this.decrease}>Decrease</button>
+      <button id="reset-btn" onClick={this.reset}>Reset</button>
     </div>
-  );
+  }
 }
 
 export default App;
